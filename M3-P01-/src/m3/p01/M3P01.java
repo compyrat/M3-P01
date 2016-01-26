a797d4d384e79e24a0da33ce8b20a60ee63d65cb
albertribgar <albertribgar@2dam-pc08.monlau.local>
2016-01-26 17:41:35 +0100

    main, Sala toString

--- M3-P01-/src/m3/p01/M3P01.java
+++ M3-P01-/src/m3/p01/M3P01.java
@@ -29,12 +29,16 @@ public class M3P01 {
         Date date = new Date();
         Pelicula pelicula1= new Pelicula("Pelicula1", "Director1", 1995, "Es una pelicula 1", Genero.COMEDIA);
         Sala sala1= new Sala(1, 90);
-        Sesion sesion= new Sesion(date, pelicula1, sala1);
+        Sesion sesion1= new Sesion(date, pelicula1, sala1);
         ArrayList<Pelicula> listaPeliculas = new ArrayList<>();
         listaPeliculas.add(pelicula1);
         ArrayList<Sala> listaSalas=new ArrayList<>();
-        
-        Cine cine = new Cine("Cinesa", "C/Falsa", listaPeliculas, listaSalas);
+        Cine cine1 = new Cine("Cinesa", "C/Falsa", listaPeliculas, listaSalas);
+        System.out.println(pelicula1.toString());
+        System.out.println(sala1.toString());
+        System.out.println(sesion1.toString());
+        System.out.println(cine1.toString());
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         //Constructores con todos los parámetros
         
         
@@ -46,7 +50,12 @@ public class M3P01 {
         
         Pelicula pelicula2=new Pelicula("Pelicula2", "Director2", 2002, "Es una pelicula 2", Genero.CIENCIA_FICCION);
         Sala sala2= new Sala(2);
-        Sesion sesion2= new Sesion(date, pelicula2, sala2);
+        Sesion sesion2= new Sesion(pelicula2, sala2, 2016, 1, 26, 17, 30);
+        Cine cine2 = new Cine("El bosque", "C/Real");
+        System.out.println(pelicula2.toString());
+        System.out.println(sala2.toString());
+        System.out.println(sesion2.toString());
+        System.out.println(cine2.toString());
         
     }
     
