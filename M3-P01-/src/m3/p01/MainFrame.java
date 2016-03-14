1a1f00ca2882f3682b0d71197018e0ea5b054fca
compyrat <compyrat95@gmail.com>
2016-03-14 16:11:02 +0100

    fix Main

--- M3-P01-/src/m3/p01/MainFrame.java
+++ M3-P01-/src/m3/p01/MainFrame.java
@@ -5,14 +5,7 @@
  */
 package m3.p01;
 
-import Exceptions.ArrayListException;
-import Exceptions.PeliculaRepetida;
-import Exceptions.SalaRepetida;
-import Exceptions.SesionSolapada;
-import java.awt.Dialog;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 import m3.p01.Panels.Cine.*;
 import m3.p01.Panels.Sesion.*;
@@ -80,15 +73,18 @@ public class MainFrame extends javax.swing.JFrame {
         jMenuSala = new javax.swing.JMenu();
         jMenuItem5 = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem14 = new javax.swing.JMenuItem();
         jMenuItem9 = new javax.swing.JMenuItem();
         jMenuItem12 = new javax.swing.JMenuItem();
         jMenuSesion = new javax.swing.JMenu();
         jMenuItem3 = new javax.swing.JMenuItem();
         jMenuItem6 = new javax.swing.JMenuItem();
+        jMenuItem15 = new javax.swing.JMenuItem();
         jMenuItem10 = new javax.swing.JMenuItem();
         jMenuItem13 = new javax.swing.JMenuItem();
         jMenuPelicula = new javax.swing.JMenu();
         jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem16 = new javax.swing.JMenuItem();
         jMenuItem8 = new javax.swing.JMenuItem();
         jMenuItem11 = new javax.swing.JMenuItem();
 
@@ -107,7 +103,6 @@ public class MainFrame extends javax.swing.JFrame {
         setBounds(new java.awt.Rectangle(0, 0, 400, 300));
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         setMinimumSize(new java.awt.Dimension(400, 300));
-        setPreferredSize(new java.awt.Dimension(400, 300));
         setResizable(false);
 
         jMenuCine.setText("Cine");
@@ -148,6 +143,14 @@ public class MainFrame extends javax.swing.JFrame {
         });
         jMenuSala.add(jMenuItem2);
 
+        jMenuItem14.setText("Consultar Salas");
+        jMenuItem14.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem14ActionPerformed(evt);
+            }
+        });
+        jMenuSala.add(jMenuItem14);
+
         jMenuItem9.setText("Modificar Sala");
         jMenuItem9.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -184,6 +187,14 @@ public class MainFrame extends javax.swing.JFrame {
         });
         jMenuSesion.add(jMenuItem6);
 
+        jMenuItem15.setText("Mostrar Sesiones");
+        jMenuItem15.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem15ActionPerformed(evt);
+            }
+        });
+        jMenuSesion.add(jMenuItem15);
+
         jMenuItem10.setText("Modificar Sesion");
         jMenuItem10.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -212,6 +223,14 @@ public class MainFrame extends javax.swing.JFrame {
             }
         });
         jMenuPelicula.add(jMenuItem4);
+
+        jMenuItem16.setText("Monstrar Cartelera");
+        jMenuItem16.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem16ActionPerformed(evt);
+            }
+        });
+        jMenuPelicula.add(jMenuItem16);
 
         jMenuItem8.setText("Modificar");
         jMenuItem8.addActionListener(new java.awt.event.ActionListener() {
@@ -249,7 +268,7 @@ public class MainFrame extends javax.swing.JFrame {
 
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
         // TODO add your handling code here:
-        if (cines.size() >= 1 ){
+        if (cines.size()>= 1 ){
             infoFail("No puedes crear mas de un cine");
         }else{
             if (jp != null){
@@ -266,6 +285,9 @@ public class MainFrame extends javax.swing.JFrame {
 
     private void jMenuItem4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem4ActionPerformed
         // TODO add your handling code here:
+        if (cines.size() < 1 ){
+            infoFail("No puedes acceder a esta opcion porque no hay ningun cine creado.");
+        }else{
         if (jp != null){
             this.remove(jp);
         }
@@ -275,10 +297,14 @@ public class MainFrame extends javax.swing.JFrame {
         this.add(cP);
         this.revalidate();
         this.repaint();
+        }
     }//GEN-LAST:event_jMenuItem4ActionPerformed
 
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
         // TODO add your handling code here:
+        if (cines.size() < 1 ){
+            infoFail("No puedes acceder a esta opcion porque no hay ningun cine creado.");
+        }else{
         if (jp != null){
             this.remove(jp);
         }
@@ -288,10 +314,14 @@ public class MainFrame extends javax.swing.JFrame {
         this.add(cSes);
         this.revalidate();
         this.repaint();
+        }
     }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     private void jMenuItem5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem5ActionPerformed
         // TODO add your handling code here:
+        if (cines.size() < 1 ){
+            infoFail("No puedes acceder a esta opcion porque no hay ningun cine creado.");
+        }else{
         if (jp != null){
             this.remove(jp);
         }
@@ -301,10 +331,14 @@ public class MainFrame extends javax.swing.JFrame {
         this.add(cS);
         this.revalidate();
         this.repaint();
+        }
     }//GEN-LAST:event_jMenuItem5ActionPerformed
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
         // TODO add your handling code here:
+        if (cines.size() < 1 ){
+            infoFail("No puedes acceder a esta opcion porque no hay ningun cine creado.");
+        }else{
         if (jp != null){
             this.remove(jp);
         }
@@ -314,10 +348,14 @@ public class MainFrame extends javax.swing.JFrame {
         this.add(aSTS);
         this.revalidate();
         this.repaint();
+        }
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     private void jMenuItem6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem6ActionPerformed
         // TODO add your handling code here:
+        if (cines.size() < 1 ){
+            infoFail("No puedes acceder a esta opcion porque no hay ningun cine creado.");
+        }else{
         if (jp != null){
             this.remove(jp);
         }
@@ -327,11 +365,14 @@ public class MainFrame extends javax.swing.JFrame {
         this.add(aPTS);
         this.revalidate();
         this.repaint();
+        }
     }//GEN-LAST:event_jMenuItem6ActionPerformed
 
     private void jMenuItem7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem7ActionPerformed
         // TODO add your handling code here:
-        
+        if (cines.size() < 1 ){
+            infoFail("No puedes acceder a esta opcion porque no hay ningun cine creado.");
+        }else{
         if (jp != null){
             this.remove(jp);
         }
@@ -341,10 +382,14 @@ public class MainFrame extends javax.swing.JFrame {
         this.add(mC);
         this.revalidate();
         this.repaint();
+        }
     }//GEN-LAST:event_jMenuItem7ActionPerformed
 
     private void jMenuItem8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem8ActionPerformed
         // TODO add your handling code here:
+        if (cines.size() < 1 ){
+            infoFail("No puedes acceder a esta opcion porque no hay ningun cine creado.");
+        }else{
         if (jp != null){
             this.remove(jp);
         }
@@ -354,10 +399,14 @@ public class MainFrame extends javax.swing.JFrame {
         this.add(mP);
         this.revalidate();
         this.repaint();
+        }
     }//GEN-LAST:event_jMenuItem8ActionPerformed
 
     private void jMenuItem9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem9ActionPerformed
         // TODO add your handling code here:
+        if (cines.size() < 1 ){
+            infoFail("No puedes acceder a esta opcion porque no hay ningun cine creado.");
+        }else{
         if (jp != null){
             this.remove(jp);
         }
@@ -367,10 +416,14 @@ public class MainFrame extends javax.swing.JFrame {
         this.add(mP);
         this.revalidate();
         this.repaint();
+        }
     }//GEN-LAST:event_jMenuItem9ActionPerformed
 
     private void jMenuItem10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem10ActionPerformed
         // TODO add your handling code here:
+        if (cines.size() < 1 ){
+            infoFail("No puedes acceder a esta opcion porque no hay ningun cine creado.");
+        }else{
         if (jp != null){
             this.remove(jp);
         }
@@ -380,10 +433,14 @@ public class MainFrame extends javax.swing.JFrame {
         this.add(mP);
         this.revalidate();
         this.repaint();
+        }
     }//GEN-LAST:event_jMenuItem10ActionPerformed
 
     private void jMenuItem11ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem11ActionPerformed
         // TODO add your handling code here:
+        if (cines.size() < 1 ){
+            infoFail("No puedes acceder a esta opcion porque no hay ningun cine creado.");
+        }else{
         if (jp != null){
             this.remove(jp);
         }
@@ -393,10 +450,14 @@ public class MainFrame extends javax.swing.JFrame {
         this.add(mP);
         this.revalidate();
         this.repaint();
+        }
     }//GEN-LAST:event_jMenuItem11ActionPerformed
 
     private void jMenuItem12ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem12ActionPerformed
         // TODO add your handling code here:
+        if (cines.size() < 1 ){
+            infoFail("No puedes acceder a esta opcion porque no hay ningun cine creado.");
+        }else{
         if (jp != null){
             this.remove(jp);
         }
@@ -406,10 +467,14 @@ public class MainFrame extends javax.swing.JFrame {
         this.add(mP);
         this.revalidate();
         this.repaint();
+        }
     }//GEN-LAST:event_jMenuItem12ActionPerformed
 
     private void jMenuItem13ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem13ActionPerformed
         // TODO add your handling code here:
+        if (cines.size() < 1 ){
+            infoFail("No puedes acceder a esta opcion porque no hay ningun cine creado.");
+        }else{
         if (jp != null){
             this.remove(jp);
         }
@@ -419,7 +484,59 @@ public class MainFrame extends javax.swing.JFrame {
         this.add(mP);
         this.revalidate();
         this.repaint();
+        }
     }//GEN-LAST:event_jMenuItem13ActionPerformed
+
+    private void jMenuItem14ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem14ActionPerformed
+        // TODO add your handling code here:
+        if (cines.size() < 1 ){
+            infoFail("No puedes acceder a esta opcion porque no hay ningun cine creado.");
+        }else{
+        if (jp != null){
+            this.remove(jp);
+        }
+        ConsultarSalas mP = new ConsultarSalas();
+        mP.setBounds(0, 0, 450, 279);
+        jp = mP;
+        this.add(mP);
+        this.revalidate();
+        this.repaint();
+        }
+    }//GEN-LAST:event_jMenuItem14ActionPerformed
+
+    private void jMenuItem15ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem15ActionPerformed
+        // TODO add your handling code here:
+        if (cines.size() < 1 ){
+            infoFail("No puedes acceder a esta opcion porque no hay ningun cine creado.");
+        }else{
+        if (jp != null){
+            this.remove(jp);
+        }
+        ConsultarSesiones mP = new ConsultarSesiones();
+        mP.setBounds(0, 0, 450, 279);
+        jp = mP;
+        this.add(mP);
+        this.revalidate();
+        this.repaint();
+        }
+    }//GEN-LAST:event_jMenuItem15ActionPerformed
+
+    private void jMenuItem16ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem16ActionPerformed
+        // TODO add your handling code here:
+        if (cines.size() < 1 ){
+            infoFail("No puedes acceder a esta opcion porque no hay ningun cine creado.");
+        }else{
+        if (jp != null){
+            this.remove(jp);
+        }
+        MostrarCartelera mP = new MostrarCartelera();
+        mP.setBounds(0, 0, 450, 279);
+        jp = mP;
+        this.add(mP);
+        this.revalidate();
+        this.repaint();
+        }
+    }//GEN-LAST:event_jMenuItem16ActionPerformed
 
     public static Boolean isNumeric(String aux){
         try{
@@ -489,6 +606,9 @@ public class MainFrame extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem11;
     private javax.swing.JMenuItem jMenuItem12;
     private javax.swing.JMenuItem jMenuItem13;
+    private javax.swing.JMenuItem jMenuItem14;
+    private javax.swing.JMenuItem jMenuItem15;
+    private javax.swing.JMenuItem jMenuItem16;
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItem4;
